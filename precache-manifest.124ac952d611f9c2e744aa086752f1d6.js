998ff18ebc933e730418eb53ab657ada780a28c0
unknown <kkiss98@gmail.com>
2024-02-08 11:04:43 +0100

    Updates

---
+++ precache-manifest.124ac952d611f9c2e744aa086752f1d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11b3780e9528b7f93f69cc486646bfe",
+    "url": "/my-app-robofriends/index.html"
+  },
+  {
+    "revision": "5c9caab680f5c9ce05b4",
+    "url": "/my-app-robofriends/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "dac5f499615b38ea6040",
+    "url": "/my-app-robofriends/static/css/main.2caacaeb.chunk.css"
+  },
+  {
+    "revision": "5c9caab680f5c9ce05b4",
+    "url": "/my-app-robofriends/static/js/2.99ae3eca.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/my-app-robofriends/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "dac5f499615b38ea6040",
+    "url": "/my-app-robofriends/static/js/main.4d3b377c.chunk.js"
+  },
+  {
+    "revision": "3a34b4ee3e1ef0acf3ff",
+    "url": "/my-app-robofriends/static/js/runtime~main.149b3a3e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/my-app-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
